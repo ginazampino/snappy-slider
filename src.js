68c10074d03fdf13c6ea09b23b190e316dfa8050
Gina <9094115+ginazampino@users.noreply.github.com>
2025-10-14 15:07:17 -0400

    Cleaning up

--- src.js
+++ src.js
@@ -19,18 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
     /* Step 2: Apply snapping to the HTML-defined target */
 
     const snapTarget = slider.getAttribute('data-snapTarget');
-    const snapAmount = parseInt(slider.getAttribute('data-snapAmount')) || 1;
     const newList = Array.from(track.children);
 
     const snapElements = newList.filter((element) => {
       return element.classList.contains(snapTarget)
     });
 
-    for (let i = 0; i < snapElements.length; i++) {
-      if (i % snapAmount === 0) {
-        snapElements[i].classList.add('snap-target');
-      };
-    };
+    snapElements.forEach((element) => {
+      element.classList.add('snap-target');
+    });
 
     /* Step 3: Add navigation functionality to the buttons */
 
@@ -55,69 +52,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
   });
 });
-
-
-
-    /* Step 2: Apply snapping */
-    // const snapItems = items.filter((child) => {
-    //   return child.classList.contains(snapTarget)
-    // });
-  
-    // snapItems.forEach((item) => {
-    //   item.classList.add('snap-target');
-    // });
-
-
-    // for (let i = items.length - 1; i > 0; i--) {
-
-    //   if (i % snapAmount === 0) {
-    //           console.log('hi')
-    //     const spacer = document.createElement('li');
-        
-    //     spacer.className = 'spacer';
-    //     spacer.setAttribute('aria-hidden', 'true');
-    //     spacer.setAttribute('role', 'presentation');
-    //     track.insertBefore(spacer, items[i]);
-    //   };
-    // };
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   document.querySelectorAll('.snappy-slider .track').forEach((track) => {
-//     const items = Array.from(track.children);
-
-//     for (let i = items.length - 1; i > 0; i--) {
-//       const spacer = document.createElement('li');
-
-//       spacer.className = 'spacer';
-//       spacer.setAttribute('aria-hidden', 'true');
-//       spacer.setAttribute('role', 'presentation');
-
-//       track.insertBefore(spacer, items[i]);
-//     };
-//   });
-
-//   document.querySelectorAll('.snappy-slider').forEach((slider) => {
-//     const content = slider.querySelector('.content');
-//     const prev = slider.querySelector('.prev');
-//     const next = slider.querySelector('.next');
-//     const scrollAmount = parseInt(slider.getAttribute('data-scrollSpeed')) || 300;
-
-//     prev.addEventListener('click', () => {
-//       content.scrollBy({
-//         left: -scrollAmount,
-//         behavior: 'smooth'
-//       });
-//     });
-
-//     next.addEventListener('click', () => {
-//       content.scrollBy({
-//         left: scrollAmount,
-//         behavior: 'smooth'
-//       });
-//     });
-//   });
-// });
-
-
